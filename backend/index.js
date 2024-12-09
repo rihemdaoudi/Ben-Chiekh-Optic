69d7f85dac8d3d9b712497a6a52b9eadadd1e16e
rihemdaoudi <rihemdaoudi42@gmail.com>
2024-12-09 23:59:08 +0100

    Add database and product model

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,59 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
-//require('dotenv').config();
+
+//Middleware
+app.use(bodyParser.json());
+//app.use(express.json());
+app.use(morgan('tiny'));
+
 
 const api = process.env.API_URL;
 
-//Middleware
-app.use(express.json());
 
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: "hair dresser",
-        image: 'some_url',
+
+
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({success: false})
     }
-    res.send(product);
+    res.send(productList);
 })
+
 app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    });
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct)
+    })).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success: false
+        });
+    });
+});
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+    dbName: 'Ben_Chiekh_Optic'
 })
+.then(() => {
+    console.log('Database Connection is ready');
+    mongoose.set('debug', true); // Enable Mongoose debug logs
+
+})
+.catch((err)=> {
+    console.log(err);
+})
+
 app.listen(3000, ()=>{
     console.log(api);
     console.log('server is running http://localhost:3000');
